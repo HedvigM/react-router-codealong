cc8729e00af87446dc022421c42d52fb18d315e1
HedvigM <hedvig@mejstedt.se>
2021-11-02 11:22:58 +0100

    album list

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import { AlbumList } from "pages/AlbumList";
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <AlbumList />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
---
+++ src/pages/AlbumList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export const AlbumList = () => {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    fetch("https://theaudiodb.com/api/v1/json/1/album.php?i=112024")
+      .then((res) => res.json())
+      .then((json) => {
+        setAlbums(json.album);
+      });
+  });
+
+  // By having an empty dependencie list we are only fetching when its mounted.
+  return (
+    <div>
+      {albums.map((album) => (
+        <div key={album.idAlbum}>
+          {/* to be able to add this slash preview we had to make the img thingie
+          a template literal... */}
+          <img src={`${album.strAlbumThumb}/preview`} alt={album.strAlbum} />
+          <h2>{album.strAlbum}</h2>
+          <h3>{album.strArtist}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
