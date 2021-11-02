8527cac83d4be9390b64d939e367d92a46cd96a1
HedvigM <hedvig@mejstedt.se>
2021-11-02 08:57:11 +0100

    cleared the documents so i can code along

--- src/App.js
+++ src/App.js
@@ -1,19 +1,1 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { AlbumList } from 'pages/AlbumList'
-import { AlbumDetail } from 'pages/AlbumDetail'
 
-export const App = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
-          <AlbumList />
-        </Route>
-        <Route path="/albums/:albumId">
-          <AlbumDetail />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  )
-}
